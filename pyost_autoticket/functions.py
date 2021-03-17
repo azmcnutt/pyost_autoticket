1018c0454e8c01bace549a280402d1dcada9de88
James M <xtfjames@gmail.com>
2021-03-17 16:01:48 -0700

    Comments and format

--- pyost_autoticket/functions.py
+++ pyost_autoticket/functions.py
@@ -1,3 +1,15 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# This file contains functions related to the pyost_autoticket  #
+# Project.  Any questions can be sent via the github project at #
+# https://github.com/azmcnutt/pyost_autoticket                  #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Project and files originaly create by James McNutt            #
+# Put into the Public Domain or the betterment of mankind 2021  #
+# https://unlicense.org                                         #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+# Returns the log level in based on the text name.  Returns the value for logging.DEBUG if no match
 def loglevel(i):
     switcher={
         'debug':10,
--- pyost_autoticket/pyost_autoticket.py
+++ pyost_autoticket/pyost_autoticket.py
@@ -1,3 +1,14 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# pyost_autoticket - automatically creates tickets in osTicket  #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Any questions can be sent via the github project at           #
+# https://github.com/azmcnutt/pyost_autoticket                  #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Project and files originaly create by James McNutt            #
+# Put into the Public Domain or the betterment of mankind 2021  #
+# https://unlicense.org                                         #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 import configparser
 import logging as l
 import functions as f
